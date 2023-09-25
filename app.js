2179e4aca41b6cdf6c5331e3af2464e59b14b7bc
matiferamador <amadormatias1999@gmail.com>
2023-09-24 21:02:50 -0300

    Proyecto terminado

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.getElementById("emailForm").addEventListener("submit", function (e) {
 });
 
 function eliminarInfo() {
-  const cardData = document.getElementsByClassName("card-info");
+  const cardData = document.getElementsByClassName("container");
 
   for (let i = 0; i < cardData.length; i++) {
     const card = cardData[i];
@@ -27,6 +27,7 @@ function eliminarInfo() {
     card.classList.remove("card-info");
 
     const success = document.createElement("div");
+
     success.classList.add("succes");
     success.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
@@ -34,9 +35,12 @@ function eliminarInfo() {
     <p> A confirmation email has been sent to
       ash@loremcompany.com. Please open it and click the button inside to confirm
       your subscription.</p>
-      <button>  Dismiss message </button>
+      <button class="btn-primary">  Dismiss message </button>
 `;
 
     card.appendChild(success);
   }
+
+  const containerSection = document.querySelector(".container.card-bg");
+  containerSection.classList.add("container2");
 }
